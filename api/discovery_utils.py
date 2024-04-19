14686b84cc9f7fe4b338a5bfb60acc245e868717
root <root@DESKTOP-JP0KK0B>
2024-04-19 14:23:54 +0200

    Shiny Front-End

--- api/discovery_utils.py
+++ api/discovery_utils.py
@@ -15,6 +15,10 @@ discovery_authenticator = BearerTokenAuthenticator(os.getenv('DISCOVERY_TOKEN'))
 discovery_url = os.getenv('DISCOVERY_URL')
 discovery_project_id = os.getenv('DISCOVERY_PROJECT_ID')
 
+DISCOVERY_TOKEN="eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImVJaWc4MFJKMEVTeW5jVXJwZUlYZWZqUXhiMnhUSGpBUXZ4YzloTm5xWk0ifQ.eyJ1aWQiOiIxMDAwMzMxMTAwIiwidXNlcm5hbWUiOiJkb2toYWxpIiwicm9sZSI6IlVzZXIiLCJwZXJtaXNzaW9ucyI6WyJjcmVhdGVfcHJvamVjdCIsImFjY2Vzc19jYXRhbG9nIiwiY3JlYXRlX3NwYWNlIiwiY2FuX3Byb3Zpc2lvbiIsInNpZ25faW5fb25seSJdLCJncm91cHMiOlsxMDAwMSwxMDAwMF0sInN1YiI6ImRva2hhbGkiLCJpc3MiOiJLTk9YU1NPIiwiYXVkIjoiRFNYIiwiYXBpX3JlcXVlc3QiOnRydWUsImlhdCI6MTcxMzUxMjEyMX0.nEjLU7Tf2RbUlmLLq7eIJYhwaT0Fw2wakytFG8ySk30TkiOZP_eXGphCh6BT4iP5c32agLXmTkrhrSfuV3LVTDUsGABZvTawDsOI9NFP2G7Uvi0tFgz456vgyJXJyhltf8N9yNWnfnla7XSjj_gqF23mIzdGUk1JxxofAqEGSumt2ANNuFohrocui-p1PNq_P5qA57VMzeU2yxPbpqI7MiroBCdZAnYiTatn5V0C4ChchaPyUkQNBrrYQTzRgQ1xmLq4wL53mv3L-3sg8gLI9Ai33TGaXQ6EPgxrbWCh9mHQlUa_RVn-Yt185XMmqC5Vt2uR7C4G7MEyzSrehQSpJQ"
+DISCOVERY_URL="https://cpd-cpd-instance.apps.aifactory-dev.dsecurecloud.de/discovery/cpd-instance-wd/instances/1703776485205351/api"
+DISCOVERY_PROJECT_ID="1013ca8b-0899-4839-ac1e-984477dcf23a"
+
 ## Initialize discovery instance ##
 discovery = DiscoveryV2(version='2023-12-28', authenticator=discovery_authenticator)
 discovery.set_service_url(
@@ -67,9 +71,8 @@ def discovery_query(nlq:str, counter=counter):
         count=counter).get_result()
     
     print("starting selection!")
-    df = pd.DataFrame.from_dict(query_results["results"])
-    df = df.fillna('')
-    return df[["title", "document_passages", "metadata"]].to_dict()
+
+    return query_results
 
 #query_results_discovery = discovery_query("welche baumaßnahmen wurden im januar 2024 durchgeführt?")
 #print()
